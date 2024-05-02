0d638388a1f6a226fc24e2e1da63f38bd0c3bca8
devwasabi <Wandile.Nyembe@bbd.co.za>
2024-05-02 10:28:51 +0200

    added foreign key constraint in V005 script

--- migrations/V005__ALTER_HISTORY.sql
+++ migrations/V005__ALTER_HISTORY.sql
@@ -1,2 +1,10 @@
 ALTER TABLE history
-ADD FOREIGN KEY user REFERENCES users (id)
+ADD CONSTRAINT fk_users_id FOREIGN KEY userid REFERENCES users(id);
+
+/*ALTER TABLE Questions
+ADD CONSTRAINT QuestionsFK FOREIGN KEY (UserId) REFERENCES Users(UserId);
+
+ALTER TABLE table1
+ADD CONSTRAINT fk_table2_id
+FOREIGN KEY (table2_id) REFERENCES table2(id);
+*/
