5196ec2c85b341933ea686f78504f181712337dd
devwasabi <Wandile.Nyembe@bbd.co.za>
2024-05-03 17:50:48 +0200

    added 2 new migration scripts

---
+++ migrations/V008__INSERT_THEMES.sql
@@ -0,0 +1,3 @@
+INSERT INTO themes (name, description) VALUES ('Light Theme', 'Light background and dark text');
+INSERT INTO themes (name, description) VALUES ('Dark Theme', 'Dark background and light text');
+INSERT INTO themes (name, description) VALUES ('Mixed Theme', 'Elements of both light and dark themes are combined');
---
+++ migrations/V009__INSERT_USERS.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (username, themepreference) VALUES ('Wandile', 1);
+INSERT INTO users (username, themepreference) VALUES ('Wayium', 2);
