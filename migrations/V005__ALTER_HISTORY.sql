f03d788049186174945bbd60710a796616cb0bf2
devwasabi <Wandile.Nyembe@bbd.co.za>
2024-05-04 21:18:21 +0200

    added unique constraint to username column

--- migrations/V005__ALTER_HISTORY.sql
+++ migrations/V005__ALTER_HISTORY.sql
@@ -1,11 +1,3 @@
 ALTER TABLE history
 ADD CONSTRAINT fk_userid
 FOREIGN KEY (userid) REFERENCES users(id);
-
-/*ALTER TABLE Questions
-ADD CONSTRAINT QuestionsFK FOREIGN KEY (UserId) REFERENCES Users(UserId);
-
-ALTER TABLE table1
-ADD CONSTRAINT fk_table2_id
-FOREIGN KEY (table2_id) REFERENCES table2(id);
-*/
