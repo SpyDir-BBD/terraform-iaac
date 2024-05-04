96f42f38ee30749d900990492b7ee8199a42563b
devwasabi <Wandile.Nyembe@bbd.co.za>
2024-05-04 10:27:13 +0200

    added 2 more insertion scripts

---
+++ migrations/V011__INSERT_HISTORY.sql
@@ -0,0 +1,8 @@
+INSERT INTO history (filename, mainfiletype, userid, datecreated)
+VALUES ('SpyDir-BBD', 1, 1, CURRENT_TIMESTAMP);
+
+INSERT INTO history (filename, mainfiletype, userid, datecreated)
+VALUES ('Django-Tutorial', 2, 1, CURRENT_TIMESTAMP);
+
+INSERT INTO history (filename, mainfiletype, userid, datecreated)
+VALUES ('Java-levelup-Tutorial', 3, 1, CURRENT_TIMESTAMP);
